daa080116e66ced6aadb36650715b169da39d4fc
Pakpoom Singkorapoom <pakpoom.singkora@gmail.com>
2024-12-17 11:29:57 +0000

    Get creative common videos
    - modify code to extract creative common videos via `get_search_cc`
    - add notebook extract_data

--- scrapetube/__init__.py
+++ scrapetube/__init__.py
@@ -1,3 +1,3 @@
-from .scrapetube import get_channel, get_search, get_playlist, get_video
+from .scrapetube import get_channel, get_search, get_playlist, get_video, get_search_cc
 
 __version__ = "2.5.1"
